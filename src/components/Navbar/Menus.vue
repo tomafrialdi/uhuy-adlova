b6dab5e2a11ca10ea26bf90f6991427f2109e307
Alexandro M <alexandro.matthew@gmail.com>
2023-06-08 22:01:21 +0700

    ui: navbar

---
+++ src/components/Navbar/Menus.vue
@@ -0,0 +1,37 @@
+<template>
+  <ul>
+    <li
+      v-for="menu in menus"
+      :key="menu.label"
+      class="font-manjari cursor-pointer"
+      @click="handleClick(menu)"
+    >
+      {{ menu.label }}
+    </li>
+  </ul>
+</template>
+
+<script setup>
+import { reactive } from "vue"
+
+const menus = reactive([
+  {
+    label: "Home",
+    to: "home",
+  },
+  {
+    label: "Service",
+    to: "service",
+  },
+  {
+    label: "About",
+    to: "about",
+  },
+])
+
+function handleClick(menu) {
+  console.log(`${menu.label} clicked`)
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/Navbar/Navburger.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="cursor-pointer lg:hidden relative">
+    <img :src="IconMenu" alt="icon-menu" @click="state.show = !state.show" />
+    <Menus
+      v-if="state.show"
+      class="absolute bg-blue-light-1 rounded-sm p-4 right-0 top-[24px] min-w-[100px]"
+    />
+  </div>
+</template>
+
+<script setup>
+import { reactive } from "vue"
+import IconMenu from "../../assets/icons/icon-menu.svg"
+import Menus from "./Menus.vue"
+
+const state = reactive({ show: false })
+</script>
+
+<style lang="scss" scoped></style>
