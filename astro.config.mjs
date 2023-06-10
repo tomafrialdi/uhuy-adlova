398fe75a56d435880be3528a3c352907896a4443
Alexandro M <alexandro.matthew@gmail.com>
2023-06-10 15:46:35 +0700

    feat: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-
-import vue from "@astrojs/vue";
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+import vue from "@astrojs/vue"
+import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue()]
-});
+  site: "https://www.adlova.com",
+  integrations: [tailwind(), vue(), sitemap()],
+})
